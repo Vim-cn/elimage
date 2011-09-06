ef91e148f5f5d26d3af58ee44961c6825db51604
lilydjwg <lilydjwg@gmail.com>
2011-09-06 13:54:50 +0800

    XHEADERS defaults to `True`

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ DEBUG = True
 DEFAULT_DATA_DIR = '/tmp'
 DEFAULT_PORT = 8888
 HOST = '' # override Host header, useful when behind another server
-XHEADERS = False # set this to true if behind another server
+XHEADERS = True # you may set this to false if not behind another server
 
 PREFIX = r'/elimage' # For WSGI. this is a regex
